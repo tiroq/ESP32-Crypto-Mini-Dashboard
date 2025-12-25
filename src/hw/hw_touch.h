cf78a36e2fc2332c4ce250302bcfca0c3c1a46c4
Ivan Shamray <ivan.shamray@exactprosystems.com>
2025-12-25 16:19:48 +0700

    Disable touch logging by setting TOUCH_DEBUG to false

--- src/hw/hw_touch.h
+++ src/hw/hw_touch.h
@@ -7,7 +7,7 @@
 // Manages XPT2046 touch controller and LVGL input device integration
 
 // Debug mode - set to true to enable touch logging
-#define TOUCH_DEBUG true
+#define TOUCH_DEBUG false
 
 bool hw_touch_init();
 
