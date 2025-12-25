8a2f824cdf2a63756f3e2ae1a3b64b063885c113
Ivan Shamray <ivan.shamray@exactprosystems.com>
2025-12-25 16:19:37 +0700

    Remove debug logging from hw_display_tick function

--- src/hw/hw_display.cpp
+++ src/hw/hw_display.cpp
@@ -64,15 +64,6 @@ bool hw_display_init() {
 }
 
 void hw_display_tick() {
-    static unsigned long last_tick_log = 0;
-    unsigned long now = millis();
-    
-    // Debug: Log every 5 seconds to verify this is being called
-    if (now - last_tick_log > 5000) {
-        Serial.println("[HW_DISPLAY] Tick running...");
-        last_tick_log = now;
-    }
-    
     // Tell LVGL that 5ms have elapsed (critical for input device polling!)
     lv_tick_inc(5);
     
