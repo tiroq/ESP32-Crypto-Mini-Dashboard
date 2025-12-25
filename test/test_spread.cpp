cb51161d8b72f9739f4b4a35282f342d4f2f82f3
Ivan Shamray <ivan.shamray@exactprosystems.com>
2025-12-25 15:23:25 +0700

    Add unit test framework for spread calculations

---
+++ test/test_spread.cpp
@@ -0,0 +1,19 @@
+// Test file for spread calculations
+// TODO: Implement unit tests
+
+#include <unity.h>
+
+void test_spread_calculation() {
+    // TODO: Add spread calculation tests
+    TEST_ASSERT_TRUE(true);
+}
+
+void setup() {
+    UNITY_BEGIN();
+    RUN_TEST(test_spread_calculation);
+    UNITY_END();
+}
+
+void loop() {
+    // Tests run once in setup
+}
